adc5fed7aa4b6ba103ae63adc26682251374d149
alexjwick <alexjwick03@gmail.com>
2025-05-17 00:27:40 -0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    // Merlinfest
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Merlinfest</h1>
+      <p className="mt-4 text-lg">A festival for all things Merlin</p>
+    </main>
+  );
+}
